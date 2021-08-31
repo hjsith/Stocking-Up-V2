e1d70f1d083c631d31ccb2000629b47e5ca0d056
Aiswarya <arajeev2001@gmail.com>
2021-08-31 23:40:42 +1000

    Added an arrow component - need to fix

---
+++ client/src/components/GreenUpArrow.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../assets/css/PortfolioPage.scss";
+
+class GreenUpArrow extends React.Component {
+  render() {
+    return <div className="ArrowUp"></div>;
+  }
+}
+
+export default GreenUpArrow;
