27a2180676730492de2dc34cf06d366c74cbc8e4
jnaris <13548565@student.uts.edu.au>
2021-09-02 16:26:22 +1000

    HTML/CSS for Company Search page added

--- client/src/pages/CompanySearch.js
+++ client/src/pages/CompanySearch.js
@@ -1,12 +1,20 @@
 import React from "react";
 import NavBar from "../components/NavBar";
+import "../assets/css/CompanySearch.scss";
 class CompanySearch extends React.Component {
   render() {
     return (
       <>
         <NavBar />
         <h1>Company Search</h1>
-      </>
+      <div class="searchbar">
+          <input type="text"
+          name="search-form"
+          id="search-form"
+          placeholder="Type a Company name or ASX Code here..."
+          />
+        </div>
+        </>
     );
   }
 }
