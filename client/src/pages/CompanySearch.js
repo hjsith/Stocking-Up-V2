b68b588be84a2ca9a73e0dc21282556745bc3ee0
jnaris <13548565@student.uts.edu.au>
2021-09-16 22:59:44 +1000

    Search bar dropdown mechanics
    Dropdown value now replaces search bar, disappears when clicked.
    
    Need to fix:
    - Navbar
    - adding links to the dropdown results
    - toggle dropdown on type, not click

--- client/src/pages/CompanySearch.js
+++ client/src/pages/CompanySearch.js
@@ -1,27 +1,50 @@
-import React, {useState} from "react";
+import React, { useState, useEffect, useRef } from "react";
 //import NavBar from "../components/NavBar"; - need to add this in, not sure where/how
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../assets/css/CompanySearch.scss";
 
+/* need to fix:
+- Navbar
+- adding links to the dropdown results
+- toggle dropdown on type, not click
+*/
+
 const SearchBar = (props) => {
-  const { options, onInputChange } = props;
+  const { results, onInputChange } = props;
+  const resultsRef = useRef()
+  const searchbarRef = useRef()
+  useEffect(() => { //when clicking outside the search bar, the dropdown is closed
+    searchbarRef.current.addEventListener("click", (event) => { 
+      event.stopPropagation(); //stop display changing to 'none' (i.e., hide) when the search bar is clicked
+      resultsRef.current.style.display = "flex"; //this is the default display (i.e., show dropdown)
+      onInputChange(event); //changes results list to include only input value
+    });
+    document.addEventListener("click", (event) => { //when clicking outside the searchbar,
+      resultsRef.current.style.display = "none";    //hide the results list (ul)
+    });
+  }, [])
   return (
     <div className="searchbar"> 
       <input 
+        id="companySearchInput"
         type="text" 
         className="form-control" 
         placeholder="Type an ASX-listed company code or name here..."
         onChange={onInputChange}
+        ref={searchbarRef}
       />
-      <ul className="list-group">
-        {options.map((option, index) => {
+      <ul id="results" className="list-group" ref={resultsRef}>
+        {results.map((result, index) => {
           return ( 
             <button 
-             type="button" 
+              type="button" 
               key={index}
-             className="list-group-item list-group-item-action"
+              onClick={(e) => { //when dropdown result is clicked, the input bar inherits the value of that result
+                searchbarRef.current.value = result;
+              }}
+              className="list-group-item list-group-item-action"
             >
-              {option}
+              {result}
             </button>
           );
         })} 
@@ -30,18 +53,19 @@ const SearchBar = (props) => {
   );
 };
 
-const dummyOptions = [] //fake data for now, an array of strings
+const dummyresults = [] //fake data for now, an array of strings
 {
-  dummyOptions.push('A2M - The A2 Milk Company');
-  dummyOptions.push('WTC - WiseTech Global');
-  dummyOptions.push('CBA - Commonwealth Bank');
+  dummyresults.push('A2M The A2 Milk Company');
+  dummyresults.push('WTC WiseTech Global');
+  dummyresults.push('CBA Commonwealth Bank');
+  dummyresults.push('ANZ Australia and New Zealand Banking Group Ltd');
 }
 
 function CompanySearch() {
-  const [options, setOptions] = useState([]);
+  const [results, setresults] = useState([]);
   const onInputChange = (event) => {
-    setOptions(
-      dummyOptions.filter((option) => option.includes(event.target.value))
+    setresults(
+      dummyresults.filter((result) => result.includes(event.target.value)) //filters through dummyresults
     );
   };
   
@@ -49,9 +73,11 @@ function CompanySearch() {
     <div className="companySearchTitle">
       <h1>Company Search</h1>
       <SearchBar 
-        options={options}
+        results={results}
         onInputChange={onInputChange} 
       />
+      <br />
+      <button className="btn btn-primary">Search</button>
     </div>
     );
 }
