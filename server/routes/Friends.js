0e8b0f5bda5944cac147906fd4ba9e514ba87e8c
jnaris <13548565@student.uts.edu.au>
2021-09-17 15:25:09 +1000

    Added Friends route

---
+++ server/routes/Friends.js
@@ -0,0 +1,14 @@
+const { Router } = require("express");
+const { StatusCodes } = require("http-status-codes");
+const { getAllCurrentFriends } = require("../daos/Friends");
+
+// Init shared
+const friends = Router();
+
+router.get("/friends", async (req, res) => {
+  const price = await getAllCurrentFriends();
+
+  return res.status(StatusCodes.OK).json(friends);
+});
+
+module.exports = router;
