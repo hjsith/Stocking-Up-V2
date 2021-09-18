8c8973e00f0e1697005fa6182dd340688cb7951f
SanyaDua <62044967+SanyaDua@users.noreply.github.com>
2021-09-18 16:23:52 +1000

    Added Hash and username Check to SignUp Route

--- server/routes/SignUp.js
+++ server/routes/SignUp.js
@@ -1,7 +1,7 @@
 const { Router } = require("express");
 const { StatusCodes } = require("http-status-codes");
-const { createInvestor } = require("../functions/Investor");
-
+const { createInvestor, checkUsernameExist } = require("../functions/Investor");
+const { bcrypt } = require("bcrypt");
 // Init shared
 const router = Router();
 
@@ -12,14 +12,17 @@ router.post("/SignUp", async (req, res) => {
       .send("The request doesn't have the correct body format.");
   }
   var data = req.body;
-
-  //Use Bcrypt package to hash password before storing
+  var checkUser = await checkUsernameExist(data.username);
+  if (checkUser === true) {
+    return res.status(StatusCodes.UNPROCESSABLE_ENTITY).send();
+  }
+  const passHash = await bcrypt.hash(data.password, 10);
 
   const user = await createInvestor(
     data.firstName,
     data.lastName,
     data.email,
-    data.password,
+    passHash,
     data.username
   );
 
