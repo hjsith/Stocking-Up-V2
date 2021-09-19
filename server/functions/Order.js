4e0b92da4f43de9a4678c8660fc1e2737cf2978a
Vishaal <vishaal.kumar@student.uts.edu.au>
2021-09-19 16:59:27 +1000

    Updates to buy and sell buttons

--- server/functions/Order.js
+++ server/functions/Order.js
@@ -1,7 +1,39 @@
-const { Order } = require("../db/Models");
+const { Order, Price } = require("../db/Models");
 
-async function getAllOrders() {
-  return await Order.findAll();
+async function getOrdersByInvestor(investorID) {
+  return await Order.findAll({
+    where: {
+      InvestorID: investorID,
+    },
+  });
 }
 
-module.exports = { getAllOrders };
+async function createOrder(
+  investorID,
+  quantityOrder,
+  orderTime,
+  typeOfOrder,
+  listingID,
+  executionTime
+) {
+  let currentPriceObject = await Price.findOne({
+    where: {
+      ListingID: listingID,
+    },
+  });
+  let orderTotal = currentPriceObject.CurrentPrice * parseInt(quantityOrder);
+
+  return await Order.create({
+    InvestorID: investorID,
+    QuantityOrder: parseInt(quantityOrder),
+    ListingPrice: currentPriceObject.CurrentPrice,
+    OrderTotal: orderTotal,
+    ExecutionTime: executionTime,
+    OrderTime: orderTime,
+    TypeOfOrder: typeOfOrder,
+    ListingID: listingID,
+    Status: typeOfOrder == "BUY" ? "PENDING" : "EXECUTED",
+  });
+}
+
+module.exports = { getOrdersByInvestor, createOrder };
