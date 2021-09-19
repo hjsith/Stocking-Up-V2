48464dd2d9c24cc454e8bd290b31c0df9e1613c9
James <james.lee9112000@gmail.com>
2021-09-19 17:42:43 +1000

    Added DAO functions for Comment and Thread models

--- server/functions/Comments.js
+++ server/functions/Comments.js
@@ -4,4 +4,89 @@ async function getAllComments() {
   return await Comments.findAll();
 }
 
-module.exports = { getAllComments };
+async function createComment(
+  inputInvestorID,
+  inputThreadID,
+  inputDateAdded,
+  inputComment,
+  inputListingPrice
+) {
+  return Comments.create({
+    InvestorID: inputInvestorID,
+    ThreadID: inputThreadID,
+    DateAdded: inputDateAdded,
+    Comment: inputComment,
+    ListingPrice: inputListingPrice,
+    Likes: 0,
+  });
+}
+
+async function increaseLike(commentID) {
+  var commentLikes = Comments.findOne({
+    attributes: ["Likes"],
+    where: {
+      CommentID: commentID,
+    },
+  });
+
+  var updatedComment = await Comments.update(
+    { Likes: commentLikes + 1 },
+    {
+      where: {
+        CommentID: commentID,
+      },
+    }
+  );
+  if (updatedComment[0] >= 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+async function decreaseLike(commentID) {
+  var commentLikes = Comments.findOne({
+    attributes: ["Likes"],
+    where: {
+      CommentID: commentID,
+    },
+  });
+
+  var updatedComment = await Comments.update(
+    { Likes: commentLikes - 1 },
+    {
+      where: {
+        CommentID: commentID,
+      },
+    }
+  );
+  if (updatedComment[0] >= 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+async function updateCommentMessage(commentID, newMessage) {
+  var updatedComment = await Comments.update(
+    { Comment: newMessage },
+    {
+      where: {
+        CommentID: commentID,
+      },
+    }
+  );
+  if (updatedComment[0] >= 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+module.exports = {
+  getAllComments,
+  createComment,
+  increaseLike,
+  decreaseLike,
+  updateCommentMessage,
+};
--- server/functions/Threads.js
+++ server/functions/Threads.js
@@ -4,4 +4,50 @@ async function getAllThreads() {
   return await Threads.findAll();
 }
 
-module.exports = { getAllThreads };
+async function findThreadsWithName(searchString) {
+  return Threads.findAll({
+    where: {
+      Username: {
+        [Op.substring]: searchString,
+      },
+    },
+  });
+}
+
+async function createThread(inputListingID, inputTitle, inputDescription) {
+  return Threads.create({
+    ListingID: inputListingID,
+    Title: inputTitle,
+    Description: inputDescription,
+  });
+}
+
+async function deleteThread(threadID) {
+  const thread = await Threads.findByPk(threadID);
+  thread.destroy();
+  return;
+}
+
+async function archiveThread(threadID, inputTitle) {
+  var updatedThread = await Threads.update(
+    { Title: inputTitle + " - Archived" },
+    {
+      where: {
+        ThreadID: threadID,
+      },
+    }
+  );
+  if (updatedThread[0] >= 1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+module.exports = {
+  getAllThreads,
+  findThreadsWithName,
+  createThread,
+  deleteThread,
+  archiveThread,
+};
