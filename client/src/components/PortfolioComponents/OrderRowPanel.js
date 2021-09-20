16cef8c0feb34e33867df4f24803b5a6f59f10ef
Aiswarya <arajeev2001@gmail.com>
2021-09-20 12:05:03 +1000

    Added a confirm button to recent orders

--- client/src/components/PortfolioComponents/OrderRowPanel.js
+++ client/src/components/PortfolioComponents/OrderRowPanel.js
@@ -6,7 +6,7 @@ class OrderRowPanel extends React.Component {
   render() {
     return (
       <div className="OrderRowPanel">
-        <table className="TableTitleFont2">
+        <table className="TableTitleFont5">
           <tr>
             <th>
               <UserProfileIcon
@@ -21,6 +21,16 @@ class OrderRowPanel extends React.Component {
             <th>{this.props.currentPrice}</th>
             <th>{this.props.units}</th>
             <th>{this.props.total}</th>
+            <th className>
+              <div className="ButtonContainer">
+                <button
+                  className="GreenConfirmButton"
+                  onClick={this.props.cancel}
+                >
+                  Confirm
+                </button>
+              </div>
+            </th>
             <th>
               <div className="ButtonContainer">
                 <button className="CancelButton" onClick={this.props.cancel}>
