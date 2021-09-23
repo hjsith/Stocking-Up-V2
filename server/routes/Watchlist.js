49a924e4628a050610b159dedfa94d69f4eb740b
Aiswarya <arajeev2001@gmail.com>
2021-09-23 15:59:53 +1000

    Added a create watch list function and updated routes to match

--- server/routes/Watchlist.js
+++ server/routes/Watchlist.js
@@ -1,14 +1,35 @@
 const { Router } = require("express");
 const { StatusCodes } = require("http-status-codes");
-const { getAllWatchlist } = require("../functions/Watchlist");
+const {
+  createWatchlist,
+  getWatchlistByInvestor,
+} = require("../functions/Watchlist");
 
 // Init shared
 const router = Router();
 
 router.get("/watchlist", async (req, res) => {
-  const watchlist = await getAllWatchlist();
+  if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .send("The request doesn't have the correct body format.");
+  }
+  let data = req.body;
+  const watchlist = await getWatchlistByInvestor(data.investorID);
 
   return res.status(StatusCodes.OK).json(watchlist);
+});
+
+router.post("/watchlist", async (req, res) => {
+  if (req.body.constructor === Object && Object.keys(req.body).length === 0) {
+    return res
+      .status(StatusCodes.BAD_REQUEST)
+      .send("The request doesn't have the correct body format.");
+  }
+  let data = req.body;
+  const watchlist = await createWatchlist(data.investorID, data.listingID);
+
+  return res.status(StatusCodes.CREATED).json(watchlist);
 });
 
 module.exports = router;
