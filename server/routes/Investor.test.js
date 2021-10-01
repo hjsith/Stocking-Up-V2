eb7e2d7994806234cafdc38a36ed69b0438fac1c
James <james.lee9112000@gmail.com>
2021-10-01 12:53:36 +1000

    Setup back-end unit testing

--- server/routes/Investor.test.js
+++ server/routes/Investor.test.js
@@ -1,22 +1,48 @@
-const { app } = require("../index");
-const { request } = require("supertest");
-const db = require("./DBInstance");
-const { getInvestor } = require("../functions/Investor");
+const app = require("../server.js");
+const request = require("supertest");
+const sequelize = require("../db/DBInstance");
+const { getAuthenticatedUserCookie } = require("../test.config");
+
+let user;
+let cookie = "";
+
+beforeAll(async () => {
+  await sequelize.sync();
+
+  // Create user in database
+  user = await request(app).post("/api/SignUp").send({
+    firstName: "Test",
+    lastName: "Tester",
+    email: "Testing@Email",
+    password: "Password",
+    username: "TestUser",
+  });
+});
+
+beforeEach(async () => {
+  cookie = await getAuthenticatedUserCookie("TestUser", "Password");
+});
 
 describe("Investor endpoint", () => {
   it("Successfully retrieved user", async () => {
-    const res = await request(app).get("/api/investor").send({
-      userID: "1ad0df57-12ae-494f-81ec-34fcc5acf38",
-    });
+    const res = await request(app)
+      .get("/api/investor?id=" + user.body.id)
+      .set("cookie", cookie);
     expect(res.status).toEqual(200);
   });
-
   it("Fail to retrieved user", async () => {
-    const res = await request(app).get("/api/investor").send({
-      userID: "Obviously not an ID",
-    });
+    const res = await request(app)
+      .get("/api/investor")
+      .set("Cookie", cookie)
+      .send({
+        userID: "Obviously not an ID",
+      });
     expect(res.status).toEqual(400);
     expect(res.body).toHaveProperty("errors");
     expect(res.body.errors).toContain("User could not be found.");
+  });
+  it("Fail on Unauthorized User", async () => {
+    const res = await request(app).get("/api/investor?id=" + user.body.id);
+    expect(res.status).toEqual(401);
   });
 });
