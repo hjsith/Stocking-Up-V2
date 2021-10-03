f821165fdbadc835c8eb1dfbfab404da454f5e01
Vishaal <vishaal.kumar@student.uts.edu.au>
2021-10-03 23:08:03 +1100

    Created article.js

---
+++ client/src/components/NewsfeedComponents/Article.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+const Article = ({ title, description, url, publishedAt, urlToImage }) => {
+  var date = publishedAt.substring(0, 10); // the date variable is substringed to only extract the part of the date that contains the YYYY/MM/DD format as the variable from the API also included the time
+  // the format for each article is made in the below section
+  return (
+    <div className="newsPanel">
+      <div className="datePanel">
+        <div className="image">
+          <img src={urlToImage} />
+        </div>
+      </div>
+
+      <div className="newsContent">
+        <h3>
+          <a href={url}>{title}</a>
+        </h3>
+        <h4>{date}</h4>
+        <p>{description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Article;
