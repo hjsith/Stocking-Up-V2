6081cf7182aaeb9c560ba6693d230ce2f84d4823
Vishaal <vishaal.kumar@student.uts.edu.au>
2021-10-15 14:13:16 +1100

    eeeee

--- server/functions/Articles.js
+++ server/functions/Articles.js
@@ -1,32 +1,26 @@
-import { Articles } from "../db/Models.js";
-import moment from "moment";
-import Axios from "axios";
+const { Articles } = require("../db/Models");
+const moment = require("moment");
+const Axios = require("axios");
 
-// this function retrieves all articles
-export async function getAllArticles() {
+async function getAllArticles() {
   return await Articles.findAll();
 }
-// this function updates articles to add to the database
-export async function updateArticles() {
+
+async function updateArticles() {
   let now = moment.utc();
   console.log(
     "Looking for new articles to add to the page!" +
       moment.utc(now, "YYYY-MM-DD HH:mm:ss").format()
   );
-
-  // Axios is used to retrieve articles using NewsApi which will retrieve articles from ABC-News with the keyword 'ASX' in the article
   const res = await Axios.get(
     "https://newsapi.org/v2/everything?q=%22ASX%22&sources=abc-news-au,%20ABC%20News%20(AU)&apiKey=a2461adc51034c3c8d7fcc374949d3c2"
   );
-  //this check is put in place to prevent duplicate articles
   for (let i = 0; i < res.data.articles.length; ++i) {
     const articleFound = await Articles.findOne({
       where: {
         ArticleName: res.data.articles[i].title,
       },
     });
-
-    // for all the articles that are not duplicates, an article is created into the database
     if (articleFound == null) {
       await Articles.create({
         ArticleName: res.data.articles[i].title,
@@ -39,3 +33,4 @@ export async function updateArticles() {
     }
   }
 }
+module.exports = { updateArticles, getAllArticles };
