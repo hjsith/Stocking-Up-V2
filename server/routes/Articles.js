5914159a9e98ded42e8cbfaf0145a3bee6e876b3
Vishaal <vishaal.kumar@student.uts.edu.au>
2021-10-15 14:12:41 +1100

    udpate

--- server/routes/Articles.js
+++ server/routes/Articles.js
@@ -1,14 +1,14 @@
-import { Router } from "express";
-import { StatusCodes } from "http-status-codes";
-import { getAllArticles } from "../functions/Articles.js";
+const { Router } = require("express");
+const { StatusCodes } = require("http-status-codes");
+const { getAllArticles } = require("../functions/Articles");
 
 // Init shared
 const router = Router();
-// this section retrieves all articles to view on the front end in the Newsfeed
+
 router.get("/articles", async (req, res) => {
   const articles = await getAllArticles();
 
   return res.status(StatusCodes.OK).json(articles);
 });
 
-export default router;
+module.exports = router;
